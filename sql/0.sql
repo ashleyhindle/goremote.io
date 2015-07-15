79aa1c50b71ea5204c7e3a2c4ce89e7148f1b193
Ashley Hindle <ashley@smellynose.com>
2015-07-15 17:17:42 +0100

    Added careers.stackoverflow to the available/enabled sources

--- sql/0.sql
+++ sql/0.sql
@@ -33,5 +33,5 @@ create table sources (
 insert into sources (sourceid, url, name, dateadded, enabled) VALUES 
 	(1, 'https://weworkremotely.com', 'WeWorkRemotely', UTC_TIMESTAMP(), 1),
 	(2, 'https://wfh.io', 'WFH.io', UTC_TIMESTAMP(), 1),
-	(3, 'https://jobs.github.com', 'GitHub Jobs', UTC_TIMESTAMP(), 1)
-	;
+	(3, 'https://jobs.github.com', 'GitHub Jobs', UTC_TIMESTAMP(), 1),
+	(4, 'https://careers.stackoverflow.com', 'StackOverflow Careers', UTC_TIMESTAMP(), 1);
---
+++ src/GoRemote/Model/StackOverflowModel.php
@@ -0,0 +1,56 @@
+<?php
+namespace GoRemote\Model;
+
+use GoRemote\Model\JobModel;
+
+class StackOverflowModel
+{
+	const SOURCE_URL = 'http://careers.stackoverflow.com/jobs/feed?allowsremote=True';
+	const SOURCE_NAME = 'stackoverflow';
+
+	private $xml;
+
+	public function __construct()
+	{
+
+	}
+
+	public function getXml()
+	{
+		return $this->xml;
+	}
+
+	public function setXml($xml)
+	{
+		$this->xml = new \SimpleXmlElement($xml);
+		return $this->xml;
+	}
+
+	public function getRss()
+	{
+		return $this->setXml(file_get_contents(self::SOURCE_URL));
+	}
+
+	public function getJobs()
+	{
+		$jobs = [];
+		foreach($this->getRss()->channel->item as $job) {
+			$jobClass = new JobModel();
+
+			$jobClass->applyurl = (string) $job->link;
+			$jobClass->position = (string) current(explode(' at ', $job->title));
+			$jobClass->dateadded = (string) (new \DateTime($job->pubDate))->format('Y-m-d H:i:s');
+			$jobClass->description = (string) $job->description;
+			$jobClass->sourceid = 4;
+			$jobClass->companyid = 99;
+			
+			preg_match('/ at (.+)\(/U', (string) $job->title, $matches);
+			$jobClass->companyname = trim($matches[1]);
+			$jobClass->companylogo = '';
+
+			$jobs[] = $jobClass;
+		}
+
+		return $jobs;
+	}
+}
