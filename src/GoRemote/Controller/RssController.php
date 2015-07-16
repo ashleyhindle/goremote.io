0e6b72ea40904dfdd345eeb8dc9354b228200ca5
Ashley Hindle <ashley@smellynose.com>
2015-07-16 18:22:08 +0100

    Added /rss

--- src/GoRemote/Controller/RssController.php
+++ src/GoRemote/Controller/RssController.php
@@ -11,7 +11,7 @@ class RssController
     {
     	$latestJobs = $app['db']->fetchAll('select jobs.*, unix_timestamp(jobs.dateadded) as dateadded_unixtime, companies.name as companyname, companies.url as companyurl, sources.name as sourcename, sources.url as sourceurl from jobs inner join companies using(companyid) inner join sources using(sourceid) where jobs.dateadded > UTC_TIMESTAMP() - INTERVAL 1 MONTH and jobs.datedeleted=0 order by jobs.dateadded desc limit 70');
     	$rss = <<<RSS
-    	<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
   <channel>
     <title>All Recent Jobs</title>
@@ -23,7 +23,7 @@ class RssController
 </rss>
 RSS;
 		// TODO Create a class to get latestjobs so we don't duplicate the query above
-		// TODO Use twig for RSS
+		// TODO Use twig for RSS as view is in the controller :(
 		foreach ($latestJobs as $job) {
 			$rss .= <<<RSS
 			<item>
@@ -39,6 +39,6 @@ RSS;
 		}
 
 
-		return new Response($rss, 201);
+		return new Response($rss, 200, array('Content-Type' => 'application/xml'));
     }
 }
