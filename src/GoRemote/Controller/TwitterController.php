73c88ec5a918598c009d2d423197acf3087112e6
Ashley Hindle <ashley@smellynose.com>
2015-07-24 14:14:30 +0100

    start of auto tweeting job posts

--- src/GoRemote/Controller/TwitterController.php
+++ src/GoRemote/Controller/TwitterController.php
@@ -17,11 +17,13 @@ class TwitterController
 
 			// store the token
 			$app['twitter']->setToken($reply->oauth_token, $reply->oauth_token_secret);
+			
 			$app['session']->set['twitter_oauth_token'] = $reply->oauth_token;
 			$app['session']->set['twitter_oauth_token_secret'] = $reply->oauth_token_secret;
 
 			// redirect to auth website
 			$auth_url = $cb->oauth_authorize();
+			return new Response($auth_url);
 			return $app->redirect($auth_url);
 		} 
         return new Response('oops');
