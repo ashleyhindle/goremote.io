4cd3571cf095cbb22b0c64bb72215b953de40301
Ashley Hindle <ashley@smellynose.com>
2015-07-27 11:33:27 +0100

    Get better stackoverflow descriptions from the page itself (not RSS)

--- src/GoRemote/Model/StackOverflowModel.php
+++ src/GoRemote/Model/StackOverflowModel.php
@@ -32,6 +32,16 @@ class StackOverflowModel implements \GoRemote\Model\SourceInterface
 		return $this->setXml(file_get_contents(self::SOURCE_URL));
 	}
 
+	public function parseDescription($link) {
+		$fc = file_get_contents($link);
+		$count = preg_match_all('/<div class="description" ?>(.+)<\/div>/mUs', $fc, $matches);
+		if (empty($count)) {
+			return false;
+		}
+
+		return implode("\n", $matches[1]);
+	}
+
 	public function getJobs()
 	{
 		$jobs = [];
@@ -43,7 +53,9 @@ class StackOverflowModel implements \GoRemote\Model\SourceInterface
 			$jobClass->applyurl = (string) $job->link;
 			$jobClass->position = (string) current(explode(' at ', $job->title));
 			$jobClass->dateadded = (string) (new \DateTime($job->pubDate))->setTimezone($tz)->format('Y-m-d H:i:s');
-			$jobClass->description = (string) $job->description;
+
+			//Description from RSS is short, get it from the link if possible
+			$jobClass->description = ($this->parseDescription((string)$job->link)) ?: (string) $job->description;
 			$jobClass->sourceid = self::SOURCE_ID;
 			
 			preg_match('/ at (.+)\(/U', (string) $job->title, $matches);
@@ -51,6 +63,8 @@ class StackOverflowModel implements \GoRemote\Model\SourceInterface
 			$jobClass->companylogo = '';
 
 			$jobs[] = $jobClass;
+			print_r($jobs);
+			exit;
 		}
 
 		return $jobs;
