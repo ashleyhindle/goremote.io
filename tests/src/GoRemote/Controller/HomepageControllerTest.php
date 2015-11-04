60fa000c74a9a916323e723aac6fb0d36bd5450c
Ashley Hindle <hey@ashleyhindle.com>
2015-11-04 00:02:00 +0000

    skip functional tests on travis?

--- tests/src/GoRemote/Controller/HomepageControllerTest.php
+++ tests/src/GoRemote/Controller/HomepageControllerTest.php
@@ -13,6 +13,12 @@ class HomepageControllerTest extends WebTestCase
 	}
 
 	public function testHomepageLoads() {
+		if (getenv('TRAVIS') == true || getenv('TRAVIS') == 'true') {
+			$this->markTestSkipped(
+				'Can\'t run functional tests on travis'
+			);
+		}
+
 		$client = $this->createClient();
 		$crawler = $client->request('GET', '/');
 
