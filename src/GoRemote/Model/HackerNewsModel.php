a188fecaa6198cb10ba0e438fdd3808f4ba18c7e
Ashley Hindle <hey@ashleyhindle.com>
2016-08-02 14:47:26 +0100

    Changes hacker news item id for August who is hiring thread

--- src/GoRemote/Model/HackerNewsModel.php
+++ src/GoRemote/Model/HackerNewsModel.php
@@ -174,6 +174,6 @@ class HackerNewsModel implements \GoRemote\Model\SourceInterface
 
 	protected function getJobsJson()
 	{
-		return $this->getFirebaseItem(12016568); // 112016568 is July
+		return $this->getFirebaseItem(12202865); // 12202865 is August
 	}
 }
