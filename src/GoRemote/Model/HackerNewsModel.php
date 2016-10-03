8c1bfbbd955e34e3b6942dd946ee051a22e76246
Ashley Hindle <hey@ashleyhindle.com>
2016-10-03 17:10:52 +0100

    Updates threadid for hacker news who is hiring post

--- src/GoRemote/Model/HackerNewsModel.php
+++ src/GoRemote/Model/HackerNewsModel.php
@@ -174,6 +174,6 @@ class HackerNewsModel implements \GoRemote\Model\SourceInterface
 
 	protected function getJobsJson()
 	{
-		return $this->getFirebaseItem(12405698); // 12405698 is September
+		return $this->getFirebaseItem(12627852); // 12627852 is October 2016
 	}
 }
