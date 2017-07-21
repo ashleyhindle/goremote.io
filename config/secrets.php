cf43be94843bebbb22dd2d730cb92a817f5f38d7
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-07-21 07:50:57 +0100

    Update secrets.php

--- config/secrets.php
+++ config/secrets.php
@@ -1,7 +1,9 @@
 <?php
 return array (
     'config.twitter' => [
-        'key' => 'YOURKEY',
-        'secret' => 'YOURSECRET',
+        'key' => 'YOURKEY', // From the developer area of twitter - for the app
+        'secret' => 'YOURSECRET', // From the developer area of twitter - for the app
+        'token' => 'OAUTH_TOKEN', // You can get this from the /twitter/auth URL
+        'token_secret' => 'OAUTH_TOKEN_SECRET', // You can get this from the twitter/auth URL
     ],
 );
